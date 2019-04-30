43c528b9a786cb9712c5e2260ac82845320cb263
Shaneil <shaneil.maharaj@yahoo.com>
2019-04-30 16:44:08 -0700

    Maps are working, getting size of maps and printing map.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,24 @@
 
 using namespace std;
 
-map<string,int> defaultMap;
+typedef map<int,pair<int,string>> defaultMap;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    defaultMap["Banana"]=15;
-    defaultMap["Apples"]=7;
+
+    defaultMap M;
+    M[1] = make_pair(10, "Bananas");
+    M[2] = make_pair(6, "Apples");
+    M[3] = make_pair(7, "Apricot");
+    M[4] = make_pair(8, "Oranges");
+    M[5] = make_pair(3, "Plums");
+    M[6] = make_pair(1, "Deez");
+    int size = M.size();
+
+
+    for (int i = 1; i <= size; i++) {
+        pair<int, string> printItem = M[i];
+        cout << "Fruit: " << printItem.second << "\t" << "Days Till Rotten: " << printItem.first << endl;
+    }
 
     return 0;
 }
