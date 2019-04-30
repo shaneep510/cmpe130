2d59d1d071335eed28137b2094f98271294d5688
Shaneil <shaneil.maharaj@yahoo.com>
2019-04-30 16:49:07 -0700

    getting proper use out of type def. working on insert function

--- main.cpp
+++ main.cpp
@@ -3,22 +3,26 @@
 
 using namespace std;
 
-typedef map<int,pair<int,string>> defaultMap;
+typedef map<int,pair<int,string>> Structure;
+Structure defaultMap;
+
+void UserInput(int,int,string) {
+    int newInsert = defaultMap.size() + 1;
+};
 
 int main() {
 
-    defaultMap M;
-    M[1] = make_pair(10, "Bananas");
-    M[2] = make_pair(6, "Apples");
-    M[3] = make_pair(7, "Apricot");
-    M[4] = make_pair(8, "Oranges");
-    M[5] = make_pair(3, "Plums");
-    M[6] = make_pair(1, "Deez");
-    int size = M.size();
+    defaultMap[1] = make_pair(10, "Bananas");
+    defaultMap[2] = make_pair(6, "Apples");
+    defaultMap[3] = make_pair(7, "Apricot");
+    defaultMap[4] = make_pair(8, "Oranges");
+    defaultMap[5] = make_pair(3, "Plums");
+    defaultMap[6] = make_pair(1, "Deez");
 
+    int size = defaultMap.size();
 
     for (int i = 1; i <= size; i++) {
-        pair<int, string> printItem = M[i];
+        pair<int, string> printItem = defaultMap[i];
         cout << "Fruit: " << printItem.second << "\t" << "Days Till Rotten: " << printItem.first << endl;
     }
 
