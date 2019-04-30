76df779b9b3b6e48e94b85172003ed7294e13328
Shaneil <shaneil.maharaj@yahoo.com>
2019-04-30 13:47:08 -0700

    First edit, testing  push and commits

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+map<string,int> defaultMap;
+
 int main() {
-    cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    defaultMap["Banana"]=15;
+    defaultMap["Apples"]=7;
+
     return 0;
 }
